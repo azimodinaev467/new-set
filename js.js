6e67c48f776d1aa0ab71f924842e5956bd25ae5a
unknown <azimodinaev467@gmail.com>
2026-06-01 15:29:24 +0500

    first commit

---
+++ js.js
@@ -0,0 +1,21 @@
+// let now = new Date();
+// console.log(now);
+
+// let date = new Date("2026-06-01");
+// console.log(date);
+
+// let date = new Date(0);
+// console.log(date);
+
+// let set = new Set([1, 2, 2, 3, 3, 3]);
+
+// console.log(set);
+
+//  let set = new Set();
+
+// set.add(1);
+// set.add(2);
+// set.add(2);
+
+// console.log(set);
+
